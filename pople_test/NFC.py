7102527f9b6821547b599fa122685b95c39e47fc
Salini <salini.1405@gmail.com>
2021-03-19 12:56:17 +0530

    Add files via upload

--- pople_test/NFC.py
+++ pople_test/NFC.py
@@ -1,4 +1,14 @@
 def NFC(at):   # returns no. of forzen core electrons  # Why not use elif??? revisit
+    """
+    Returns the number of frozen core electrons in the atom
+
+            Parameters:
+                    at (char): Symbol of the element
+
+            Returns:
+                    NHC (int): Number of frozen core electrons in the atom
+    """
+
     if at in ['H','He']:
         return(0)
     if at in ['Li','Be','B','C','N','O','F','Ne','Na','Mg']:
