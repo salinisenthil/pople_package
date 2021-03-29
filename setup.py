88c5a87038ce1f82f04ab3e818f8744e2ae5a2c6
Salini <salini.1405@gmail.com>
2021-03-29 12:28:27 +0530

    Add files via upload

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '1.0.0'
+VERSION = '1.1.0'
 PACKAGE_NAME = 'pople'
-AUTHOR = 'Salini Senthil'
+AUTHOR = 'Salini Senthil, Raghunathan Ramakrishnan'
 AUTHOR_EMAIL = 'salinis@tifrh.res.in'
 URL = 'https://github.com/salinisenthil/pople'
 
-LICENSE = 'Apache License 2.0'
-DESCRIPTION = 'Describe your package in one sentence'
+LICENSE = 'MIT License'
+DESCRIPTION = 'A toolkit for ab initio thermochemistry'
 LONG_DESCRIPTION = (HERE / "README.md").read_text()
 LONG_DESC_TYPE = "text/markdown"
 
@@ -27,6 +27,8 @@ setup(name=PACKAGE_NAME,
       license=LICENSE,
       author_email=AUTHOR_EMAIL,
       url=URL,
+      include_package_data = True,
       install_requires=INSTALL_REQUIRES,
-      packages=find_packages()
+      #packages = ['.','templates'],
+      packages=find_packages(),
       )
