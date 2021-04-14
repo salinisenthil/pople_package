6968e19219d3e8d0a5a28a95cd3a90f1d3cc5e2e
Salini <salini.1405@gmail.com>
2021-04-14 17:06:53 +0530

    Add files via upload

---
+++ pople/tests/test_010_methods_comparison_C6H6_parallel/README.txt
@@ -0,0 +1,3 @@
+This calculation requires declaring the environment variables for mpirun as listed in the file parallel_env.sh
+
+
---
+++ pople/tests/test_010_methods_comparison_C6H6_parallel/parallel_env.sh
@@ -0,0 +1,3 @@
+export OMP_NUM_THREADS=1
+export PATH=/apps/openmpi-3.0.0_install/bin:$PATH
+export LD_LIBRARY_PATH=/apps/openmpi-3.0.0_install/lib:$LD_LIBRARY_PATH
