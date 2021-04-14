e2e55a3e335710c8ac0b0d700d7aa06d63354930
Salini <salini.1405@gmail.com>
2021-04-14 14:47:35 +0530

    Add files via upload

--- pople/tests/test_005_protonaffinity_NH3/inp.py
+++ pople/tests/test_005_protonaffinity_NH3/inp.py
@@ -1,34 +1,48 @@
 import os
 from pople import calculator as calc
-from pople import au2ev
+from pople import au2ev, au2kcm, au2kjm
 
 #=== Remove old files
 os.system("rm -f ORCA.com ORCA.out Thermochemistry.out input.*")
 
 exe='/home/Lib/ORCA_420/orca'
 
-#=== neutral
-geom_1 = '''
-0  2
-Cl  0.0  0.0  0.0
-'''
-#=== anion
-geom_2 = '''
--1  1
-Cl  0.0  0.0  0.0
+#=== NH3
+geom = '''
+           0           1
+ N        0.00000000     0.00000000     0.00000000
+ H       -0.39793000     0.00000000    -0.93852000
+ H       -0.39793000    -0.81279000     0.46926000
+ H       -0.39793000     0.81279000     0.46926000
 '''
 
-out1 = calc(code='orca', code_exe=exe, method='g4mp2', xyz=geom_1)
-out2 = calc(code='orca', code_exe=exe, method='g4mp2', xyz=geom_2)
+out = calc(code='orca', code_exe=exe, method='g4mp2-xp', xyz=geom)
+H_mol=out[2]
 
-U0_neutral=out1[0]
-U0_anion=out2[0]
+#=== NH4+
+geom = '''
+           1           1
+ N        0.00000000     0.00000000     0.00000000
+ H        0.59435200     0.59435200     0.59435200
+ H       -0.59435200    -0.59435200     0.59435200
+ H        0.59435200    -0.59435200    -0.59435200
+ H       -0.59435200     0.59435200    -0.59435200
+'''
 
-EA = (U0_neutral-U0_anion) * au2ev
+out = calc(code='orca', code_exe=exe, method='g4mp2-xp', xyz=geom)
+H_cation_protonated=out[2]
 
-print(' Electron affinity of Cl atom is is ', EA,' eV')
+PA = H_mol - H_cation_protonated
+
+print(' Standard proton affinity (298.15 K) of NH3 is ', (PA), ' hartree')
+print(' Standard proton affinity (298.15 K) of NH3 is ', (PA)*au2ev, ' eV')
+print(' Standard proton affinity (298.15 K) of NH3 is ', (PA)*au2kcm, ' kcal/mol')
+print(' Standard proton affinity (298.15 K) of NH3 is ', (PA)*au2kjm, ' kJ/mol')
 
 #Reference output
-#Electron affinity of Cl atom is is  3.6421649462081915  eV
-#Exp. value is 3.6166 eV
+#Standard proton affinity (298.15 K) of NH3 is  0.322931995145062  hartree
+#Standard proton affinity (298.15 K) of NH3 is  8.787427911725857  eV
+#Standard proton affinity (298.15 K) of NH3 is  202.64291773307903  kcal/mol
+#Standard proton affinity (298.15 K) of NH3 is  847.8579677952026  kJ/mol
+#Exp. value is 202.5 kcal/mol
 
